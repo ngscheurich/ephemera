3e8b582762eacdae1832ff323012933e068b7de5
Nicholas Scheurich <nick@scheurich.me>
2018-02-28 13:57:44 -0600

    Add Spotify tracks changed subscription

--- lib/ephemera_web/schema.ex
+++ lib/ephemera_web/schema.ex
@@ -10,4 +10,11 @@ defmodule EphemeraWeb.Schema do
       resolve &Resolvers.Content.spotify_recently_played/3
     end
   end
+
+  subscription do
+    @desc "Get notified when new recently played Spotify tracks are available"
+    field :spotify_tracks_changed, list_of(:spotify_track) do
+      resolve &Resolvers.Content.spotify_recently_played/3
+    end
+  end
 end
