8ca67c8b0401942446ebf2ddcc1d8af1f863def9
Nicholas Scheurich <nick@scheurich.me>
2018-02-28 16:19:58 -0600

    Remove comments

--- lib/ephemera_web/endpoint.ex
+++ lib/ephemera_web/endpoint.ex
@@ -4,16 +4,10 @@ defmodule EphemeraWeb.Endpoint do
 
   socket "/socket", EphemeraWeb.UserSocket
 
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phoenix.digest
-  # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :ephemera, gzip: false,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
-  # Code reloading can be explicitly enabled under the
-  # :code_reloader configuration of your endpoint.
   if code_reloading? do
     plug Phoenix.CodeReloader
   end
@@ -29,9 +23,6 @@ defmodule EphemeraWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
 
-  # The session will be stored in the cookie and signed,
-  # this means its contents can be read but not tampered with.
-  # Set :encryption_salt if you would also like to encrypt it.
   plug Plug.Session,
     store: :cookie,
     key: "_ephemera_key",
